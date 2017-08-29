9dd4b1e5fbfe6bcef11e29ac92673ee7f56d832b
Chris <cstar1107@gmail.com>
2017-08-29 21:49:37 +1000

    Added style.css and test.js

---
+++ test.js
@@ -0,0 +1,45 @@
+var test = {
+	heldShapes: [],
+	renderTest: function() {
+		// I
+		playingField[0][0] = "cyan";
+		playingField[0][1] = "cyan";
+		playingField[0][2] = "cyan";
+		playingField[0][3] = "cyan";
+
+		// J
+		playingField[19][9] = "blue";
+		playingField[19][8] = "blue";
+		playingField[19][7] = "blue";
+		playingField[18][7] = "blue";
+
+		playingField[17][1] = "blue";
+		playingField[18][1] = "blue";
+		playingField[19][1] = "blue";
+		playingField[19][0] = "blue";
+
+		// colours
+		playingField[2][0] = "orange";
+		playingField[2][1] = "purple";
+		playingField[2][2] = "yellow";
+		playingField[2][3] = "green";
+		playingField[2][4] = "red";
+		playingField[2][5] = "blue";
+		playingField[2][6] = "cyan";
+	},
+	lineClearTest: function() {
+		for (var i = 0; i < 9; i++) {
+			playingField[16][i] = "red";
+			playingField[17][i] = "red";
+			playingField[18][i] = "red";
+			playingField[19][i] = "red";
+		}	
+	},
+	loseConditionTest: function() {
+		for (var i = 8; i < playingField.length; i++) {
+			for (var j = 0 ; j < playingField[i].length - 1; j++) {
+				playingField[i][j] = "red";
+			}
+		}
+	},
+};
